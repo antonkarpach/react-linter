34c4257dd7b3a300c965b4e3079acf3efb290759
Karpach <anton.karpach@airnow.com>
2022-04-01 13:31:24 +0300

    Add tested values for linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Test from './components/Test';
 
 function App() {
+  const [state, setState] = useState<number | null>();
+  useEffect(() => {
+    setState(null);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {JSON.stringify(state)}
+        <Test
+          textProcessNumberOne="Every day like"
+          textProcessNumberTwo="A new string "
+          textProcessNumberThree="Nobody knows"
+          textProcessNumberFour="Where it will"
+          textProcessNumberFive="Transitions us"
+        />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Amet deserunt esse libero voluptatum. Atque deleniti dicta ea enim eos
-          inventore maxime modi mollitia non quia quos, sed tempora temporibus voluptate?
+          Edit <code>src/App.tsx</code> and save to reload. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Amet deserunt esse libero voluptatum. Atque deleniti dicta ea enim eos inventore maxime modi mollitia non quia
+          quos, sed tempora temporibus voluptate?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/components/Test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface TestProps {
+  textProcessNumberOne: string;
+  textProcessNumberTwo: string;
+  textProcessNumberThree: string;
+  textProcessNumberFour: string;
+  textProcessNumberFive: string;
+}
+
+const Test: React.FC<TestProps> = ({
+  textProcessNumberOne,
+  textProcessNumberTwo,
+  textProcessNumberThree,
+  textProcessNumberFour,
+  textProcessNumberFive,
+}) => {
+  return (
+    <div>
+      <span>{textProcessNumberOne}</span>
+      <br />
+      <span>{textProcessNumberTwo}</span>
+      <br />
+      <span>{textProcessNumberThree}</span>
+      <br />
+      <span>{textProcessNumberFour}</span>
+      <br />
+      <span>{textProcessNumberFive}</span>
+    </div>
+  );
+};
+
+export default Test;
